4644b82901ca8112b6a36a626f3259482340ed23
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-05 14:13:48 +0200

    sync screen osZoomFactor/pixelRatio with atex-web-ext; unsure if it's *more* accurate

--- src/screen.js
+++ src/screen.js
@@ -66,27 +66,23 @@ export let screen = {
     })
   },
 
-  // Windows: it's the same thing as DPI zoom (usually between 100-200%%)
   osZoomFactor: shouldThrottle(function() {
     if (_.isDefined(cfg.screen.osZoomFactor)) {
       return cfg.screen.osZoomFactor;
     }
 
-    let osZoomFactor = window.devicePixelRatio /
-      (cfg.page.zoomFactor * screen.pixelRatio());
-    return _.round(osZoomFactor, 2);
+    if (_.includes(navigator.appVersion, 'Win')) {
+      // it's the same thing as DPI zoom (usually between 100-200%%)
+      let pixelRatio = window.devicePixelRatio / cfg.page.zoomFactor;
+      return _.round(pixelRatio, 2);
+    }
+
+    return 1;
   }),
 
   pixelRatio: shouldThrottle(function() {
     if (_.isDefined(cfg.screen.pixelRatio)) {
       return cfg.screen.pixelRatio;
-    }
-
-    if (_.includes(navigator.appVersion, 'Win')) {
-      // Windows always has window.devicePixelRatio = 1 at rest
-      // i.e. when page zoom is 100% and
-      // OS zoom (Display Settings -> Change size...) is also 100%
-      return 1;
     }
 
     let pixelRatio = window.devicePixelRatio / cfg.page.zoomFactor;
