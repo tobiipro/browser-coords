7fc85bca11427aa0c6162fa9dcf82b05ebf2ac97
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-22 18:55:45 +0200

    reorder

--- src/cfg.js
+++ src/cfg.js
@@ -1,15 +1,12 @@
 export let cfg = {
-  page: {
-    // Page zoom set by user
-    // E.g. retrieve this from a WebExtension
-    zoomFactor: undefined
-  },
+  throttle: undefined,
 
-  client: {
-    // e.g. this will be guestimated automatically in the top window,
-    // but it has to be set by the caller in iframes
-    x: undefined,
-    y: undefined
+  screen: {
+    // e.g. set via external means, otherwise this will be guestimated
+    osZoomFactor: undefined,
+
+    // e.g. set via external means, otherwise this will be guestimated
+    pixelRatio: undefined
   },
 
   window: {
@@ -20,15 +17,18 @@ export let cfg = {
     }
   },
 
-  screen: {
-    // e.g. set via external means, otherwise this will be guestimated
-    osZoomFactor: undefined,
-
-    // e.g. set via external means, otherwise this will be guestimated
-    pixelRatio: undefined
+  client: {
+    // e.g. this will be guestimated automatically in the top window,
+    // but it has to be set by the caller in iframes
+    x: undefined,
+    y: undefined
   },
 
-  throttle: undefined
+  page: {
+    // Page zoom set by user
+    // E.g. retrieve this from a WebExtension
+    zoomFactor: undefined
+  }
 };
 
 export default cfg;
